4fbbb3e3ad490ff2aff92b8904ca18087dd31e80
Igor Monteiro <igornathandev@gmail.com>
2022-06-20 10:38:59 -0300

    feat: aws-s3 upload

--- src/config/upload-multer.ts
+++ src/config/upload-multer.ts
@@ -1,9 +1,10 @@
 import { Request } from 'express'
-import aws from 'aws-sdk'
+import { S3Client } from '@aws-sdk/client-s3'
 import multerS3 from 'multer-s3'
 import multer from 'multer'
 import path from 'path'
 import crypto from 'crypto'
+
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'tmp', 'uploads')
 
@@ -12,24 +13,29 @@ const storageTypes = {
     destination: uploadFolder,
     filename(req: Request, file, cb) {
       const hash = crypto.randomBytes(16).toString('hex')
-      const filename = `${hash}-${file.originalname}`
+      file.key = `${hash}-${file.originalname}`
 
-      return cb(null, filename)
+      return cb(null, file.key)
     },
   }),
-  s3: multerS3
-}
-
-export default {
-  storage: multer.diskStorage({
-    destination: uploadFolder,
-    filename(req: Request, file, cb) {
+  s3: multerS3({
+    s3: new S3Client({
+      region: 'us-east-1',      
+    }),
+    bucket: 'nodejs-portfolio',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl : 'public-read',
+    key: (req: Request, file, cb) => {
       const hash = crypto.randomBytes(16).toString('hex')
       const filename = `${hash}-${file.originalname}`
 
       return cb(null, filename)
-    },
-  }),
+    }
+  })
+}
+
+export default {
+  storage: storageTypes['s3'],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
