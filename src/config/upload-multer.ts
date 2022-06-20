50533aee9db282c4c4f1da5ec7391b88d42f67f3
Igor Monteiro <igornathandev@gmail.com>
2022-06-20 09:53:44 -0300

    chore: add @types/multer-s3

--- src/config/upload-multer.ts
+++ src/config/upload-multer.ts
@@ -1,9 +1,24 @@
 import { Request } from 'express'
+import aws from 'aws-sdk'
+import multerS3 from 'multer-s3'
 import multer from 'multer'
 import path from 'path'
 import crypto from 'crypto'
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'tmp', 'uploads')
+
+const storageTypes = {
+  local: multer.diskStorage({
+    destination: uploadFolder,
+    filename(req: Request, file, cb) {
+      const hash = crypto.randomBytes(16).toString('hex')
+      const filename = `${hash}-${file.originalname}`
+
+      return cb(null, filename)
+    },
+  }),
+  s3: multerS3
+}
 
 export default {
   storage: multer.diskStorage({
