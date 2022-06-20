3e7c54af78fa0ee8b5205377fbc8360f2365d2ef
Igor Monteiro <igornathandev@gmail.com>
2022-06-20 11:50:22 -0300

    feat: POST routes using multer as a middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from 'express'
+import { Post } from './models/post'
 import multer from 'multer'
 import multerConfig from './config/upload-multer'
 
@@ -6,10 +7,31 @@ const uploadMulter = multer(multerConfig)
 
 const routes = Router()
 
-routes.post('/uploads', uploadMulter.single('file'), (req: Request, res: Response) => {
-  console.log(req.file)
+routes.get("/posts", async (req: Request, res: Response) => {
+  const posts = await Post.find();
 
-  return res.json({ message: 'Upload with NodeJS' })
+  return res.json(posts);
+});
+
+routes.post('/uploads', uploadMulter.single('file'), async (req: Request, res: Response) => {
+  const { originalname: name, size, key, location: url = '' } = req.file
+
+  const post = await Post.create({
+    name,
+    size,
+    key,
+    url
+  })
+
+  return res.json(post)
 })
+
+routes.delete("/posts/:id", async (req: Request, res: Response) => {
+  const post = await Post.findById(req.params.id);
+
+  await post.remove();
+
+  return res.send();
+});
 
 export { routes }
