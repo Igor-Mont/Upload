51feafa10689eb6e44a09027b28cc28dcfbccb9b
Igor Monteiro <igornathandev@gmail.com>
2022-06-20 11:04:04 -0300

    feat: static files in local upload

--- src/http.ts
+++ src/http.ts
@@ -2,11 +2,14 @@ import 'dotenv/config'
 import express, { json } from 'express'
 import cors from 'cors'
 import { routes } from './routes'
+import path from 'path'
 
 const app = express()
 
 app.use(cors())
 app.use(json())
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
 
 app.use(routes)
 
